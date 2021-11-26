87b3d9d98415c8caf3dc01b20a437838084efb55
pankajganhotra021 <pankajganhotra021@gmail.com>
2021-11-26 16:17:37 +0530

    db url in env

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/spike-tasks',
+    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/spike-tasks',
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
